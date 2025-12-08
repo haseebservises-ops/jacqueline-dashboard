5b88a78c16f3962fcd2682473604f095620ff1a0
Haseeb <haseebservises@gmail.com>
2025-12-08 05:10:54 +0500

    feat: add 10 more emailjs accounts (total 14)

--- src/config/emailKeys.js
+++ src/config/emailKeys.js
@@ -3,32 +3,83 @@
  * 
  * The system will randomly pick one of these accounts for every email.
  * If one fails (e.g. quota limit reached), it will automatically try another one.
+ * 
+ * Total Accounts: 14
+ * Estimated Monthly Capacity: 2,800 emails (at 200/account)
  */
 
 export const EMAIL_POOL = [
-    // Account 1 (Generic)
+    // --- BATCH 1 (Added previously) ---
     {
         serviceId: 'service_558cphs',
         templateId: 'template_6dhgaaw',
         publicKey: 'wXvJlNCb9dYZFu7ck'
     },
-    // Account 2 (mon840830)
     {
         serviceId: 'service_sfi4ofe',
         templateId: 'template_honqbhj',
-        publicKey: 'SLMsMn-WcjPQCTh9y'
+        publicKey: 'SLMsMn-WcjPQCTh9y' // mon840830
     },
-    // Account 3 (Contact Us)
     {
         serviceId: 'service_at7j2nt',
         templateId: 'template_6h25enw',
         publicKey: 'cNbQGcv0mlbc7et2g'
     },
-    // Account 4 (ah0063725)
     {
         serviceId: 'service_xliz6wh',
         templateId: 'template_0d2rziq',
-        publicKey: 'de1Blu50SkfII7uNQ'
+        publicKey: 'de1Blu50SkfII7uNQ' // ah0063725
     },
-    // Add more accounts here...
+
+    // --- BATCH 2 (Added recently) ---
+    {
+        serviceId: 'service_9o4oz3l',
+        templateId: 'template_g17s485',
+        publicKey: '0le35qV0rO7xsEUNY' // team.haseeb.chatgpt
+    },
+    {
+        serviceId: 'service_zhfez8s',
+        templateId: 'template_i7gccse',
+        publicKey: 'Xojy0Aop1rb9Sy8xE' // mahboss000
+    },
+    {
+        serviceId: 'service_0uqyjok',
+        templateId: 'template_xbhmqwt',
+        publicKey: 'UzPERALRYA6mXax7m' // areej.ghl.services
+    },
+    {
+        serviceId: 'service_eotujom',
+        templateId: 'template_ypji7s5',
+        publicKey: 'q5nKrQ7VqYNVYya2L' // haseeb.love.usa
+    },
+    {
+        serviceId: 'service_urokqng',
+        templateId: 'template_7cl5eb8',
+        publicKey: 'bcoOAzTOzCXalaxxc' // haseebservices48
+    },
+    {
+        serviceId: 'service_9gvxfqr',
+        templateId: 'template_1a93ohl',
+        publicKey: 'w1_3VS9S-mnb1l2_i' // haseeb.ghl.pro
+    },
+    {
+        serviceId: 'service_ustyhe8',
+        templateId: 'template_mgiua5a',
+        publicKey: 'bBtqXw3Bz_4pdsM1B' // javariamohsin1
+    },
+    {
+        serviceId: 'service_k60vnng',
+        templateId: 'template_rnnawan',
+        publicKey: 'saTV43E7tbv39p4Hq' // fishhookmarketing
+    },
+    {
+        serviceId: 'service_6net1oc',
+        templateId: 'template_ktk3nae',
+        publicKey: 'qkN_9dLqZsNsLuyMW' // khurramsaeed424
+    },
+    {
+        serviceId: 'service_zrg5maj',
+        templateId: 'template_e6ie8hc',
+        publicKey: 'BhskVSZ4xO_ahMky7' // umair.razzaq381
+    },
 ];
